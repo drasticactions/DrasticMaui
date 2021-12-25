409f93a984e0cb26edf0eaae596957946f27eb33
Tim Miller <innerlogic4321@gmail.com>
2021-12-25 15:48:08 -0500

    Add Multi Window for Catalyst

---
+++ DrasticMaui.Sample/Platforms/MacCatalyst/SceneDelegate.cs
@@ -0,0 +1,13 @@
+﻿using Foundation;
+using Microsoft.Maui;
+using ObjCRuntime;
+using UIKit;
+
+namespace DrasticMaui.Sample
+{
+	[Register("SceneDelegate")]
+	public class SceneDelegate : MauiUISceneDelegate
+	{
+	}
+}
+
