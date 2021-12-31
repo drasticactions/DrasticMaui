2cc3a06cb1d3a62aed57d4fa34eef5e01e2cb852
Tim Miller <innerlogic4321@gmail.com>
2021-12-31 12:04:46 -0500

    Add full screen for Mac

--- DrasticMaui/DrasticMauiWindow.MacCatalyst.cs
+++ DrasticMaui/DrasticMauiWindow.MacCatalyst.cs
@@ -5,8 +5,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using Foundation;
+using ObjCRuntime;
 using UIKit;
 
 namespace DrasticMaui
@@ -29,6 +32,48 @@ namespace DrasticMaui
             }
 
             // TODO: Get NSWindow.
+
+            var nsApplication = Runtime.GetNSObject(Class.GetHandle("NSApplication"));
+            if (nsApplication is null)
+            {
+                return;
+            }
+
+            var sharedApplication = nsApplication.PerformSelector(new Selector("sharedApplication"));
+            if (sharedApplication is null)
+            {
+                return;
+            }
+
+            var applicationDelegate = sharedApplication.PerformSelector(new Selector("delegate"));
+            if (applicationDelegate is null)
+            {
+                return;
+            }
+
+            var nsWindowHandle = IntPtr_objc_msgSend_IntPtr(applicationDelegate.Handle, Selector.GetHandle("hostWindowForUIWindow:"), window.Handle);
+            var nsWindow = Runtime.GetNSObject(nsWindowHandle);
+            if (nsWindow is null)
+            {
+                return;
+            }
+
+            void_objc_msgSend_bool(nsWindowHandle, Selector.GetHandle("toggleFullScreen:"), fullScreen);
         }
+
+        [DllImport("/usr/lib/libobjc.dylib", EntryPoint = "objc_msgSend")]
+        private static extern IntPtr IntPtr_objc_msgSend_nfloat(IntPtr receiver, IntPtr selector, nfloat arg1);
+
+        [DllImport("/usr/lib/libobjc.dylib", EntryPoint = "objc_msgSend")]
+        private static extern IntPtr IntPtr_objc_msgSend_IntPtr(IntPtr receiver, IntPtr selector, IntPtr arg1);
+
+        [DllImport("/usr/lib/libobjc.dylib", EntryPoint = "objc_msgSend")]
+        private static extern IntPtr IntPtr_objc_msgSend(IntPtr receiver, IntPtr selector);
+
+        [DllImport("/usr/lib/libobjc.dylib", EntryPoint = "objc_msgSend")]
+        private static extern void void_objc_msgSend_IntPtr(IntPtr receiver, IntPtr selector, IntPtr arg1);
+
+        [DllImport("/usr/lib/libobjc.dylib", EntryPoint = "objc_msgSend")]
+        private static extern void void_objc_msgSend_bool(IntPtr receiver, IntPtr selector, bool arg1);
     }
 }
